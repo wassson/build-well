08377fb6aee4f7cd25079d590fda641e98ed41a9
Austin Wasson <austinpwasson@gmail.com>
2023-12-28 20:14:34 +0000

    ▲ Add Vercel CI job
    “**This branch adds a new CI job for Vercel.** It's powered by the [@pierre/vercel](https://www.npmjs.com/package/@pierre/vercel) CI recipe and once setup will automatically generate preview and production deployments automatically on push. Preview builds are linked through a blue gauge at the top right hand side of your branch pages (if you don't see this yet, and instead just see a giant red "x" CI gauge, it just means you need to connect your Vercel account below). By default this CI integration will also do a production deploy after every merge to main!
    
    ![](https://db.heypierre.app/storage/v1/object/public/description-images/c883b76d-44f1-4401-a0e6-8bd3ce3da556-CleanShot%202023-12-08%20at%2016.04.09@2x.png)
    
    ## Connecting to vercel
    
    To finish setting up the Vercel CI integration, just click the "Connect Vercel" button below. In the following screen, be sure to select the correct team and repo for your vercel project.
    
    Once completed, the button above should say "Connected" and your preview gauge should automatically start building. If you run into any issues at all please reach out to the Pierre team on [discord](https://discord.gg/QJtTu5hpzx). We're here to help!…”
    
    Read more: https://pierre.co/build-well/marketing/pierre-ci-vercel-riib6

---
+++ .pierre/ci/vercel.ts
@@ -0,0 +1,8 @@
+import { Job } from "@pierre/recipes/vercel";
+
+export const label = "Vercel";
+
+export default Job({
+  VERCEL_ORG_ID: "ZlQvtvAe5g9adT8HCWyj2sgZ",
+  VERCEL_PROJECT_ID: "prj_u9szOE7iQ1rY7At9M3oECadPq8D7",
+});
